3dde4f96ba7697968326678b016bd3794b0b13f7
yulinyu <2508000341@QQ.COM>
2025-04-01 15:23:21 +0800

    1

---
+++ ssm0q94t/src/main/webapp/admin/src/utils/base.js
@@ -0,0 +1,16 @@
+const base = {
+    get() {
+        return {
+            url : "http://localhost:8080/ssm0q94t/",
+            name: "ssm0q94t",
+            // 退出到首页链接
+            indexUrl: 'http://localhost:8080/ssm0q94t/front/index.html'
+        };
+    },
+    getProjectName(){
+        return {
+            projectName: "购物系统小程序"
+        } 
+    }
+}
+export default base
---
+++ ssm0q94t/src/main/webapp/front/api/base.js
@@ -0,0 +1,4 @@
+﻿const base = {
+        url : "http://localhost:8080/ssm0q94t/"
+    }
+export default base
